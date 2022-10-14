f5905d7adcaaaf7499fb54d2a18fe3e0b1a72afb
HzJandex <115798306+HzJandex@users.noreply.github.com>
2022-10-14 20:35:08 +0300

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,16 @@
+#Ne yepacağınızı bilemem ancak proje yok :)
+
+
+import random
+
+lower = "abcçdefgğhıijklmnoöprsştuüvyz"
+upper = lower.upper()
+number = "123456789"
+mark = "*!_"
+
+
+password = random.choices(total , k=10)
+print(password)
+
+password = ''.join(password)
+print(password)
